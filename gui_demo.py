7031b68021b6888a96bce1abbdb1f854679d1ec1
章政兴 <zhangzhengxing@hys-inc.cn>
2019-08-09 17:11:11 +0800

    demo of svd algorithm in image compression.

--- gui_demo.py
+++ gui_demo.py
@@ -4,54 +4,72 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+import matplotlib.image as mpimg
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageDraw 
+import svd_image
 
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.file = ''
         self.wm_title("Embed matplotlib in tkinter")
         self.createWidgets()
 
     def createWidgets(self):
-        fig = Figure(figsize=(5,4),dpi = 100)
-        self.ax = fig.add_subplot(111)
+        fig = Figure(figsize=(5,2),dpi = 100)
+        
+        self.ax1 = fig.add_subplot(1,2,1)
+        self.ax2 = fig.add_subplot(1,2,2)
+
         self.canvas = FigureCanvasTkAgg(fig,master=self)
         self.canvas.get_tk_widget().pack(side = tk.TOP,fill = tk.BOTH,expand =1)
         self.canvas._tkcanvas.pack(side = tk.TOP,fill = tk.BOTH,expand =1)
         toolbar = NavigationToolbar2TkAgg(self.canvas,self)
         toolbar.update()
         footframe = tk.Frame(master = self).pack(side=tk.BOTTOM)
-        tk.Button(master  = footframe,text = 'redraw',command = self.draw).pack(side = tk.BOTTOM)
-        tk.Button(master=footframe,text='exit',command=self._quit).pack(side = tk.BOTTOM)
+        tk.Button(master=footframe,text='convert',command=self.convert).pack(side = tk.BOTTOM)
+        self.entry = tk.Entry(master=footframe, show=None, font=('Arial', 14))
+        self.entry.pack(side = tk.BOTTOM)
 
         main_menu = tk.Menu(self)
 
         file_menu = tk.Menu(main_menu,tearoff = 0)
 
-        file_menu.add_command(label = 'Open',command = self._open_file)
+        file_menu.add_command(label = 'Open',command = self.open_file)
         file_menu.add_command(label = 'Exit', command = self._quit)
         main_menu.add_cascade(label = 'File',menu = file_menu)
         self.config(menu = main_menu)
-
-        self.draw()
-
-    def draw(self):
-        x = np.random.randint(0,50,size = 100)
-        y = np.random.randint(0,50,size = 100)
-        self.ax.clear()
-        self.ax.scatter(x,y,s = 3)
-        self.canvas.show()
 
     def _quit(self):
         self.quit()
         self.destroy()
 
-    def _open_file(self):
+    def open_file(self):
         file = tk.filedialog.askopenfilename() 
-        image = Image.open(file) 
-        image.show()
+        if (file != ''):
+            self.file = file
+            img_eg = mpimg.imread(file)
+            self.ax1.imshow(img_eg)
+            #.join(img_eg.shape)
+            self.ax1.set(title = ("src:"+str(tuple(img_eg.shape))))
+            self.canvas.draw()
+    
+    def convert(self):
+        if (self.file != ''):
+            sval = int(self.entry.get())
+            if (sval > 0):
+                data = mpimg.imread(self.file)
+                real_sval, transformed_data = svd_image.svd_compress(data, sval)
+                self.ax2.imshow(transformed_data.astype(np.uint8))
+                shape = transformed_data.shape
+                if len(shape) == 2:
+                    shape[1] = shape[1] * shape[2]
+                    shape[2] = 1
+                ratio = (shape[0] + shape[1]+1) * real_sval/ float(shape[0] * shape[1])
+                self.ax2.set(title = ("compress ratio:{:.4f}".format(ratio)))
+                self.canvas.draw()
 
 if  __name__ == '__main__':
     app = Application()
--- svd_image.py
+++ svd_image.py
@@ -2,36 +2,30 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-def compress(file_name="/Users/Mac/Downloads/test.jpg"):
-    img_eg = mpimg.imread(file_name)
-    img_tmp = img_eg.reshape(img_eg.shape[0],img_eg.shape[1]*img_eg.shape[2])
-    U,Sigma,VT = np.linalg.svd(img_tmp)
-    sval_num = 10
-    img_restruct1 = (U[:,0:sval_num]).dot(np.diag(Sigma[0:sval_num])).dot(VT[0:sval_num,:])
-    img_restruct1 = img_restruct1.reshape(img_eg.shape[0],img_eg.shape[1],img_eg.shape[2])
+'''
+    -Dimension must be 2 or 3.
+'''
+def svd_compress(data,sval):
+    if data.all() == None:
+        raise Exception('None Data.')
 
+    shape = data.shape
+    shape_size = len(data.shape)
+    if ((shape_size < 2) or (shape_size > 3)):
+        raise Exception('Dimension is:'+str(len(data.shape)) + 'must be 2 or 3.')
+ 
+    if shape_size == 3:
+        data = data.reshape(data.shape[0],data.shape[1] * data.shape[2])
+        
 
-    sval_num = 60
-    img_restruct2 = (U[:,0:sval_num]).dot(np.diag(Sigma[0:sval_num])).dot(VT[0:sval_num,:])
-    img_restruct2 = img_restruct2.reshape(img_eg.shape[0],img_eg.shape[1],img_eg.shape[2])
-
-
-    sval_num = 200
-    img_restruct3 = (U[:,0:sval_num]).dot(np.diag(Sigma[0:sval_num])).dot(VT[0:sval_num,:])
-    img_restruct3 = img_restruct3.reshape(img_eg.shape[0],img_eg.shape[1],img_eg.shape[2])
-
-    fig, ax = plt.subplots(2,2,figsize = (24,32))
+    '''The sval must be less than shape[0]'''
+    if sval > data.shape[0]:
+        sval = data.shape[0]
     
-    ax[0][0].imshow(img_eg)
-    ax[0][0].set(title = "src")
+    U,Sigma,VT = np.linalg.svd(data)
+       
+    img_restruct = (U[:,0:sval]).dot(np.diag(Sigma[0:sval])).dot(VT[0:sval,:])
 
-    ax[0][1].imshow(img_restruct1.astype(np.uint8))
-    ax[0][1].set(title = "nums of sigma = 10")
-
-    ax[1][0].imshow(img_restruct2.astype(np.uint8))
-    ax[1][0].set(title = "nums of sigma = 60")
-
-    ax[1][1].imshow(img_restruct3.astype(np.uint8))
-    ax[1][1].set(title = "nums of sigma = 200")
-    plt.show()
-  
+    if len(shape) == 3:
+        img_restruct = img_restruct.reshape(shape[0],shape[1],shape[2])
+    return sval,img_restruct
